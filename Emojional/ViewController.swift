c8dea37a8e10aeef91a838d866b2cfb7915f0ece
Alencia <alencia@Apples-MacBook-Pro-18.local>
2019-08-12 10:02:32 -0700

    message

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -9,21 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let emojis = ["🤩": "super excited!!", "🥳": " ready to party!"]
+    
     override func viewDidLoad(){
         super.viewDidLoad()
     }
+    
     @IBAction func showMessage(sender: UIButton){
-        let alertController = UIAlertController(title:"This is a test", message: "If you see this, it works!", preferredStyle: UIAlertController.Style.alert)
-        func showMessage(sender:UIButton){
+    
+        if let selectedEmotion = sender.titleLabel?.text{
+            let titleText = "Welcome to my project"
+            let messageText = (emojis [selectedEmotion]!)
+            
+            let alertController = UIAlertController(title: "\(titleText)", message: "\(messageText)", preferredStyle: UIAlertController.Style.alert)
+            
+    
             alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            
             present(alertController, animated:true, completion: nil )
         }
     }
+    
+}
     
         // Do any additional setup after loading the view.
 
 
 
-}
+
 
